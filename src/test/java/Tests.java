2b494f2cb5dc759f63c605ca8c0c3a93ed70a1f2
jukkty <dima.iachimov@gmail.com>
2021-05-19 10:23:49 +0300

    final commit for sure

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -22,7 +22,6 @@ class Tests {
 
     @BeforeEach
     void setUp() {
-//        Не могу понять смысл добавления нижестоящего кода? С ним даже как-то кривее работает.
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -41,12 +40,12 @@ class Tests {
     void shouldTestWithNormalData() throws InterruptedException {
         driver.get("http://localhost:7777");
         Thread.sleep(1000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Якимов Дмитрий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        WebElement form = driver.findElement(By.cssSelector("form[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Якимов Дмитрий");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("span[class=button__text]")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
         Thread.sleep(1000);
     }
@@ -56,12 +55,12 @@ class Tests {
     void shouldTestIfWrongName() throws InterruptedException {
         driver.get("http://localhost:7777");
         Thread.sleep(1000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Iachimov Dmitri");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
+        WebElement form = driver.findElement(By.cssSelector("form[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Iachimov Dmitri");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("span[class=button__text]")).click();
+        String text = driver.findElement(By.cssSelector("span[class=input__sub]")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
         Thread.sleep(1000);
     }
@@ -70,11 +69,11 @@ class Tests {
     void shouldTestIfWrongNumber() throws InterruptedException {
         driver.get("http://localhost:7777");
         Thread.sleep(1000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Якимов Дмитрий");
-        elements.get(1).sendKeys("321313131313331");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button__text")).click();
+        WebElement form = driver.findElement(By.cssSelector("form[class='form form_size_m form_theme_alfa-on-white']"));
+        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Якимов Дмитрий");
+        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("3131313131311");
+        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        form.findElement(By.cssSelector("span[class=button__text]")).click();
         String text = driver.findElement(By.cssSelector("#root > div > form > div:nth-child(2) > span > span > span.input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
         Thread.sleep(1000);
